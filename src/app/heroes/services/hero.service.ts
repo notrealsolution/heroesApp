940a8afd5cd717bd4965e901e7f40b3bf0188ec4
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-04-27 09:06:13 -0600

    Obtención del heroe por url

--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environments } from 'environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -11,5 +11,9 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+  getHeroById ( id: string ): Observable< Hero | undefined > {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe( catchError( error => of (undefined)));
   }
 }
