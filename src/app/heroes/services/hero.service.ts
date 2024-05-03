2819d668124da978617176d2ec7ff5f712f2628b
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-03 14:53:48 -0600

    Iniciado con el CRUD

--- src/app/heroes/services/hero.service.ts
+++ src/app/heroes/services/hero.service.ts
@@ -21,6 +21,19 @@ export class HeroesService {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`).pipe(
       map((heroes: Hero[]) => heroes.filter(hero => hero.superhero.toLowerCase().includes(query.toLowerCase())))
     );
-
+  }
+  addHero( hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+  updateHero( hero: Hero): Observable<Hero> {
+    if( !hero.id ) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+  deleteHeroById( id:string ): Observable<boolean> {
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(false)),
+        map( resp => true )
+      );
   }
 }
