039cc9e10fe0170197e79808f3ca9182147706ee
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-04 05:24:46 -0600

    Implementando Form Reactivo

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class NewPageComponent {
+
+  public formCotrol = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', { nonNullable: true }),
+    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
+    alt_imag:        new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics'}
