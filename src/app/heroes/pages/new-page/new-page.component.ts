a0b230c1e04affe94ce50ed5f22682f21c41b1d6
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-04 06:08:53 -0600

    Recuperando info del formulario

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-new-page',
@@ -25,10 +26,13 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics'}
   ];
 
+  public constructor ( private heroService: HeroesService){}
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
   onSumit():void {
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    });
+    if( this.heroForm.invalid ) return;
+
   }
 }
