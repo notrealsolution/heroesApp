254d4741fdcca67e5592c26aeb4f52b092e0083f
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-04 05:47:51 -0600

    Conectando formulario reactivo

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -10,7 +10,7 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
-  public formCotrol = new FormGroup({
+  public heroForm = new FormGroup({
     id:               new FormControl<string>(''),
     superhero:        new FormControl<string>('', { nonNullable: true }),
     publisher:        new FormControl<Publisher>( Publisher.DCComics ),
@@ -24,4 +24,11 @@ export class NewPageComponent {
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics'}
   ];
+
+  onSumit():void {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    });
+  }
 }
