e2ac4877485753d72c29aac54d64fb0a6b7208a7
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-05-10 20:22:52 -0600

    Implementando las funciones canActivate y canMatch

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard {
+export class AuthGuard implements CanMatch, CanActivate{
   constructor() { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable< boolean >  {
+    console.log('Can Match')
+    console.log( { route, state } )
+    return true;
+  }
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable< boolean >  {
+    console.log('Can Activate')
+    console.log( {route, segments})
+    return true;
+  }
 
 }
